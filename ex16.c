03de231ee4664eb945129d852e24649697b31781
Johan Alexandersson <johal855@student.liu.se>
2017-09-20 18:43:47 -0700

    Complete extra credit for Ex16

--- ex16.c
+++ ex16.c
@@ -10,62 +10,65 @@ struct Person{
 	int weight;
 };
 
-struct Person *Person_create(char *name, int age, int height, int weight)
+struct Person Person_create(char *name, int age, int height, int weight)
 {
-	struct Person *who = malloc(sizeof(struct Person));
-	assert(who != NULL);
+	struct Person who;
 
-	who->name = strdup(name);
-	who->age = age;
-	who->height = height;
-	who->weight = weight;
+	who.name = strdup(name);
+	who.age = age;
+	who.height = height;
+	who.weight = weight;
 
 	return who;
 }
 
-void Person_destroy(struct Person *who)
+void Person_destroy(struct Person who)
 {
-	assert(who != NULL);
 
-	free(who->name);
-	free(who);
+
+	free(who.name);
+	
 }
 
-void Person_print(struct Person *who)
+void Person_print(struct Person who)
 {
-	printf("Name: %s.\n", who->name);
-	printf("Age: %d.\n", who->age);
-	printf("Height: %d.\n", who->height);
-	printf("Weight: %d.\n", who->weight);
+	printf("Name: %s.\n", who.name);
+	printf("Age: %d.\n", who.age);
+	printf("Height: %d.\n", who.height);
+	printf("Weight: %d.\n", who.weight);
 }
 
 int main(int argc, char *argv[])
 {
 	// make two people structures
-	struct Person *joe = Person_create("Joe Alex", 32, 64, 140);
 
-	struct Person *frank = Person_create("Frank Blank", 20, 72, 180);
+	struct Person joe = Person_create("Joe Alex", 32, 64, 140);
+	
+	printf("Here I am, rock you like a hurricane.\n");
+	
+	struct Person frank = Person_create("Frank Blank", 20, 72, 180);
 
 	// print them out and where they are in memory
-	printf("Joe is at memory location %p:\n", joe);
+	printf("Joe is at memory location %p:\n", &joe);
 	Person_print(joe);
 
-	printf("Frank is at memory location %p:\n", frank);
+	printf("Frank is at memory location %p:\n", &frank);
 	Person_print(frank);
 
+	
 	// make everyone age 20 years and print again
-	joe->age += 20;
-	joe->height -= 2;
-	joe->weight += 40;
+	joe.age += 20;
+	joe.height -= 2;
+	joe.weight += 40;
 	Person_print(joe);
 
-	frank->age = 20;
-	frank->age += 20;
+	frank.age = 20;
+	frank.age += 20;
 	Person_print(frank);
 
 	// destroy both so we clean up
 	Person_destroy(joe);
 	Person_destroy(frank);
-
+	
 	return 0;
 }
