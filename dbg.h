cc7207d123a7812212712d5fd62261819a9c8b22
Johan Alexandersson <johal855@student.liu.se>
2017-09-26 10:58:59 +0200

    Fix output format

--- dbg.h
+++ dbg.h
@@ -8,7 +8,7 @@
 #ifdef NDEBUG
 #define debug(M, ...)
 #else
-#define debug(M, ...) fprintf(stderr, "DEBUG %s:%d: " M "\n",\
+#define debug(M, ...) fprintf(stderr, "[DEBUG] %s:%d: " M "\n",\
 	__FILE__, __LINE__, ##__VA_ARGS__)
 #endif
 
@@ -22,7 +22,7 @@
 	"[WARN] (%s:%d: errno:%s" M "\n",\
 	__FILE__, __LINE__, clean_errno(), ##__VA_ARGS__)
 
-#define log_info(M, ...) fprintf(stderr, " [INFO] (%s:%d) " M "\n",\
+#define log_info(M, ...) fprintf(stderr, "[INFO] (%s:%d) " M "\n",\
 	__FILE__, __LINE__, ##__VA_ARGS__)
 
 #define check(A,M, ...) if(!(A)) {\
